2c87267248c6a994233d8abfb29afdbb0633cb28
devadg <opponamer@gmail.com>
2024-02-02 13:12:15 +0100

    fixed loader

--- app/loading.js
+++ app/loading.js
@@ -1,9 +1,18 @@
 export default function loading() {
     return (
-        <div className="h-screen w-full text-white">
+        <div className="h-screen w-full text-white relative">
             <div className="w-full h-full flex justify-center flex-col items-center">
-                <h1 className="headertin text-3xl">ADG.</h1>
-                <p className="text-xl leading-10">Loading UI...</p>
+                <h1 className="headertin text-3xl md:text-6xl fixed top-5 left-10">ADG.</h1>
+                <div class="loader">
+                    <p>loading</p>
+                    <div class="words">
+                    <span class="word">Interface</span>
+                    <span class="word">Dataset</span>
+                    <span class="word">Graphics</span>
+                    <span class="word">Aesthet</span>
+                    <span class="word">Operation</span>
+                    </div>
+                </div>
             </div>
         </div>
     );
